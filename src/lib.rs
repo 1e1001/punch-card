859bf9a860e105fb9ef8b514b579454d3890635e
1e1001 <michael@malinov.com>
2022-03-08 13:13:48 -0800

    (web) hopeful #3: this doesn't work

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,49 @@
-#![feature(const_fn_trait_bound)]
+#![feature(generic_associated_types)]
+#![feature(generic_const_exprs)]
 use std::ops::{RangeFull, RangeTo, RangeToInclusive};
 
 enum StripValue {
 	Zero, One, End
 }
 trait Strip<const N: usize> {
-	type Inner: Strip<N - 1>;
+	type Inner: Strip<{N - 1}> where [(); N - 1]: Sized;
 	const VALUE: StripValue;
 }
 impl<const N: usize> Strip<N> for () {
-	type Inner = ();
+	type Inner = () where [(); N - 1]: Sized;
 	const VALUE: StripValue = StripValue::End; 
 }
 impl Strip<1> for RangeFull {
 	type Inner = ();
 	const VALUE: StripValue = StripValue::End;
 }
-impl<const N: usize, T: Strip<N - 1>> Strip<N> for RangeTo<T> {
+impl<const N: usize, T: Strip<{N - 1}>> Strip<N> for RangeTo<T> {
 	type Inner = T;
 	const VALUE: StripValue = StripValue::Zero;
 }
-impl<const N: usize, T: Strip<N - 1>> Strip<N> for RangeToInclusive<T> {
+impl<const N: usize, T: Strip<{N - 1}>> Strip<N> for RangeToInclusive<T> {
 	type Inner = T;
 	const VALUE: StripValue = StripValue::One;
 }
 
-const fn eval_strip<const N: usize, T: Strip<N>>(_strip: &T) {
+const fn eval_strip_type<const N: usize, T: Strip<N>>() {
 	match <T>::VALUE {
-		StripValue::Zero => {},
-		StripValue::One => {},
-		StripValue::End => {},
+		StripValue::Zero => {
+		    print!("0");
+		    eval_strip_type::<{N - 1}, <T>::Inner>()
+		},
+		StripValue::One => {
+		    print!("0");
+		    eval_strip_type::<{N - 1}, <T>::Inner>()
+		},
+		StripValue::End => {
+		    println!(".");
+		},
 	}
+}
+
+const fn eval_strip<const N: usize, T: Strip<N>>(_: &T) {
+    eval_strip_type::<N, T>();
 }
 
 #[cfg(test)]
